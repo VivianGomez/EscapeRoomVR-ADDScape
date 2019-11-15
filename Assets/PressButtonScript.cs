09931b9960d21d7ded94ba28c0e3323a30a47000
jcsanguino10 <vn.gomez@uniandes.edu.co>
2019-11-15 17:54:19 -0500

    Animaciones botones

---
+++ Assets/PressButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButtonScript : MonoBehaviour
+{
+    public Animator anim;
+
+    IEnumerator OnTriggerEnter()
+    {
+        anim.SetBool("press", true);
+        yield return new WaitForSeconds(2);
+        anim.SetBool("press", false);
+    }
+}
