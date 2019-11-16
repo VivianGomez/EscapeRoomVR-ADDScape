d98ae88ec8ab33c385b4d91a59c5a78a940a07e1
jcsanguino10 <vn.gomez@uniandes.edu.co>
2019-11-15 19:19:11 -0500

    Ya sirven los botones, suenan y brillan

---
+++ Assets/Scripts/BtnPressedInteraction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnPressedInteraction : MonoBehaviour
+{
+    public int btnID;
+    public float intensidadLuz;
+    public Light Luz;
+    public bool desactivando;
+    public bool desactivado;
+    public AudioClip sonido;
+    //public ControladorJuego controlador;
+
+    //Color baseColor;
+    //Renderer renderer_;
+    //Material mat;
+    //float emission;
+
+    void Start()
+    {
+        intensidadLuz = Luz.intensity; 
+    }
+
+    void Activar(){
+        desactivado = false;
+        desactivando = false;
+        Luz.intensity = intensidadLuz;
+        Luz.gameObject.SetActive(true);
+
+        // controlador. 
+        AudioSource.PlayClipAtPoint(sonido, Vector3.zero, 1.0f);
+
+        Invoke("Desactivar", 0.1f);
+    }
+
+    void Desactivar(){
+        desactivando = true;
+    }
+
+    void Update()
+    {
+        if(desactivando && !desactivado){
+            Luz.intensity = Mathf.Lerp(Luz.intensity, 0, 0.065f);
+        }
+
+        if(Luz.intensity <= 0.02)
+        {
+            Luz.intensity = 0;
+            desactivado = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Activar();
+
+    }
+}
