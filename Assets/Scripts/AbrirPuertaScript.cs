98e7ba08d9f229ce158cee1baf0adadaec4deb22
Vivian <vn.gomez@uniandes.edu.co>
2019-11-17 15:52:23 -0500

    Se puede abrir la puerta con la llave correcta

---
+++ Assets/Scripts/AbrirPuertaScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirPuertaScript : MonoBehaviour
+{
+    public bool completada = false;
+    public AudioClip push;
+    public AudioClip incorrecto;
+
+    public PlayAnimationAbrirPuerta playAnimationAbrirPuerta;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.name == this.name)
+        {
+            AudioSource.PlayClipAtPoint(push, Vector3.zero, 1.0f);
+            completada = true;
+            StartCoroutine(playAnimationAbrirPuerta.AnimPlay());
+            Destroy(col.gameObject);
+        }
+        else{
+            AudioSource.PlayClipAtPoint(incorrecto, Vector3.zero, 1.0f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayAnimationAbrirPuerta.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayAnimationAbrirPuerta : MonoBehaviour
+{
+    public Animator anim;
+
+    public IEnumerator AnimPlay()
+    {
+        anim.SetBool("abrir", true);
+        yield return new WaitForSeconds(1);
+        //anim.SetBool("abrir", false);
+    }
+}
