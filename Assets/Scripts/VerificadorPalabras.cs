23fa95c1b608952de63892193dcbeed5e0b6ba4c
jcsanguino10 <vn.gomez@uniandes.edu.co>
2019-11-19 15:14:18 -0500

    avance juego palabras, se verifica que en cada bloque se ponga la letra correcta para armar la palabra

--- Assets/Scripts/VerificadorPalabras.cs
+++ Assets/Scripts/VerificadorPalabras.cs
@@ -25,12 +25,12 @@ public class VerificadorPalabras : MonoBehaviour
     public void Jugar()
     {
         char[] arr = palabra.ToCharArray(0, palabra.Length);
-
         for (int i = 0; i < arr.Length; i++)
         {
             bloques[i] = Instantiate(gOPrefab, new Vector3((xI += espacio), yI, zI), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
             //gOModelo.transform.localScale += new Vector3(3f, 3f, 3f);
             bloques[i].name = "" + arr[i];
+            bloques[i].AddComponent<VerificarCaracter>();
         }
     }
 
@@ -42,4 +42,6 @@ public class VerificadorPalabras : MonoBehaviour
             bloques.Add(gOPrefab);
         }
     }
+
+
 }
---
+++ Assets/Scripts/VerificarCaracter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerificarCaracter : MonoBehaviour
+{
+    public bool completada = false;
+    public AudioClip correcto;
+    public AudioClip incorrecto;
+
+    private void Start()
+    {
+        correcto = Resources.Load<AudioClip>("Sonidos/positive-beeps");
+        incorrecto = Resources.Load<AudioClip>("Sonidos/negative-beeps");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        print(col.name);
+
+        if (((col.name.ToLower()) == this.name) || (col.name == ("block-"+this.name)))
+        {
+            AudioSource.PlayClipAtPoint(correcto, Vector3.zero, 1.0f);
+            completada = true;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            AudioSource.PlayClipAtPoint(incorrecto, Vector3.zero, 1.0f);
+            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+}
