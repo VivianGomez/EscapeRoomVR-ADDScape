dd5daefd834e1e1bbd96d541474a5310bd8ebce8
jcsanguino10 <vn.gomez@uniandes.edu.co>
2019-11-19 12:46:40 -0500

    avance juego palabras, se crean los bloques para cada letra de la palabra dada por parámetro

---
+++ Assets/Scripts/EmpezarJuegoPalabras.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpezarJuegoPalabras : MonoBehaviour
+{
+
+    public float intensidadLuz;
+    public Light Luz;
+    public AudioClip sonido;
+    public VerificadorPalabras controlador;
+    public Animator anim;
+    public GameObject btnJugar;
+
+    private bool pTouched = false;
+
+    void Start()
+    {
+        intensidadLuz = Luz.intensity;
+        pTouched = false;
+    }
+
+    IEnumerator AnimPlay()
+    {
+        anim.SetBool("press", true);
+        yield return new WaitForSeconds(1);
+        anim.SetBool("press", false);
+    }
+
+    public void Activar()
+    {
+        Luz.intensity = intensidadLuz;
+        Luz.gameObject.SetActive(true);
+        StartCoroutine(AnimPlay());
+        AudioSource.PlayClipAtPoint(sonido, Vector3.zero, 1.0f);
+        btnJugar.SetActive(false);
+        controlador.Jugar();
+    }
+
+    void OnTriggerEnter()
+    {
+
+        /**if (!pTouched)
+        {
+            pTouched = true;*/
+        Activar();
+        /**    yield return new WaitForSeconds(3);    
+            pTouched = false;    
+            print("Off "+ pTouched);
+        }*/
+    }
+
+}
---
+++ Assets/Scripts/VerificadorPalabras.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VerificadorPalabras : MonoBehaviour
+{
+
+    private List<GameObject> bloques;
+    public GameObject gOPrefab;
+    public string palabra;
+    public float xI;
+    public float yI;
+    public float zI;
+    public float espacio = 0.3f;
+
+
+    void Start()
+    {
+        bloques = new List<GameObject>();
+        LlenarGameObjectsVacios();
+    }
+
+
+    public void Jugar()
+    {
+        char[] arr = palabra.ToCharArray(0, palabra.Length);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            bloques[i] = Instantiate(gOPrefab, new Vector3((xI += espacio), yI, zI), Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+            //gOModelo.transform.localScale += new Vector3(3f, 3f, 3f);
+            bloques[i].name = "" + arr[i];
+        }
+    }
+
+
+    public void LlenarGameObjectsVacios()
+    {
+        for (int i = 0; i < palabra.Length; i++)
+        {
+            bloques.Add(gOPrefab);
+        }
+    }
+}
